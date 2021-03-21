89e980be091cc2273110a1eb253df06dd9f49ce3
Victor Hugo <57298432+victorugob@users.noreply.github.com>
2021-03-20 22:22:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the beggining
+          This is the beggining 2
         </p>
         <a
           className="App-link"
